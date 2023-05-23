f80ba559c59bfea0204eaba7eaacc8734c8d3dac
Goushith <goushithcmohan@gmail.com>
2023-05-23 19:26:53 +0530

    task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlidebarComponent } from './slidebar/slidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RightNavComponent } from './right-nav/right-nav.component';
+import { TeacherdashboardComponent } from './teacherdashboard/teacherdashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlidebarComponent,
+    DashboardComponent,
+    RightNavComponent,
+    TeacherdashboardComponent
   ],
   imports: [
     BrowserModule,
